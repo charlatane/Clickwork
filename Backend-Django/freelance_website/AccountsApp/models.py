168ee43fb50523a61edb5891b8a37e16989df93b
samihanuva <samihanuva2@gmail.com>
2021-10-15 23:17:29 +0600

    critical issues solved

--- Backend-Django/freelance_website/AccountsApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class AccountTypeModel(models.Model):
-   Username = models.TextField(primary_key=True)
-   AccountType = models.TextField()
-   
